62db8a2aad605d2e11da9d4b2fa57e80857cbd81
Mangar <Mangar@RYZEN9>
2025-01-18 23:52:59 +0100

    mit evades in quiescense.

--- search/searchparameter.h
+++ search/searchparameter.h
@@ -92,7 +92,7 @@ namespace QaplaSearch {
 
 		static const bool QUIESCENSE_USE_SEE_PRUNINT = false;
 		static const bool USE_HASH_IN_QUIESCENSE = false;
-		static const bool EVADES_CHECK_IN_QUIESCENSE = false;
+		static const bool EVADES_CHECK_IN_QUIESCENSE = true;
 		static const value_t PRUING_SAFETY_MARGIN_IN_CP = 50;
 
 		static const bool DO_MOVE_ORDERING_STATISTIC = false;
